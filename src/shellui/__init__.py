bd72b95c5557a920d56fd6a4012aea0c1f4d6b44
Sw3aty <72179940+Sw3aty-Acc@users.noreply.github.com>
2024-09-15 20:11:57 +0300

    add shellui\__init__.py package

---
+++ src/shellui/__init__.py
@@ -0,0 +1,16 @@
+"""
+shellboard - cross-platform framework that facilitates the development of a graphical interface for the command shell
+            Credits:
+        Author - highofolly
+        Source - https://github.com/highofolly/shellboard
+         Email - sw3atyspace@gmail.com
+        GitHub - https://github.com/highofolly
+Discord Server - https://discord.com/invite/jchJKYqNmK
+       Youtube - https://www.youtube.com/@sw3aty702
+"""
+
+__version__ = "0.1a1"
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG, filename="shellui_debug.log", filemode="w", format="%(asctime)s %(levelname)s %(message)s")
