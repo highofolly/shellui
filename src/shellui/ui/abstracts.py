ed9a7a9999884b878ae0a26425cd9970d0bdc030
Sw3aty <72179940+Sw3aty-Acc@users.noreply.github.com>
2024-09-17 15:38:13 +0300

    refactor: change events/add debug messages

--- src/shellui/ui/abstracts.py
+++ src/shellui/ui/abstracts.py
@@ -1,3 +1,4 @@
+from .. import logging
 from ..core.overloads import ABC, abstractmethod
 from ..core.handler import EventManager
 from ..core.typings import Buffer, List
@@ -8,10 +9,14 @@ class BaseElement(ABC):
         self.event = EventManager()
         self.event.create.build(self.build)
         self.event.create.update(self.update)
+        self.event.create.key_pressed(self.key_pressed)
+        logging.debug(f"<{__name__}>: create class {self.__class__} (agrs={args}, kwargs={kwargs})")
 
     @abstractmethod
     def build(self) -> Buffer:
         raise NotImplementedError
+
+    def key_pressed(self, key_char: int) -> None: ...
 
     def update(self) -> None: ...
 
@@ -20,15 +25,10 @@ class AbstractWidget(BaseElement):
     def __init__(self, *args, **kwargs):
         super(AbstractWidget, self).__init__(*args, **kwargs)
         self.event.create.output(self.output)
-        self.event.create.on_hover(self.on_hover)
-        self.event.create.on_pressed(self.on_pressed)
 
     @abstractmethod
     def output(self) -> str:
         raise NotImplementedError
-
-    def on_hover(self) -> None: ...
-    def on_pressed(self) -> None: ...
 
     def build(self) -> Buffer:
         return Buffer(self.event.call.output)
@@ -38,7 +38,11 @@ class AbstractLayout(BaseElement):
     def __init__(self, *args, **kwargs):
         super(AbstractLayout, self).__init__(*args, **kwargs)
         self.elements: List[BaseElement] = []
+        self.cursor: int = 0
         self.event.create.adjust(self.adjust)
+
+    def key_pressed(self, key_char: int) -> None:
+        self.elements[self.cursor].event.call.key_pressed(key_char)
 
     @abstractmethod
     def adjust(self, buffer: Buffer) -> str:
