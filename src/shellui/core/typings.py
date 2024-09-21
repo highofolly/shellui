416c3dfbbf5cd5d780c82d7aa8905cb563b1cf83
Sw3aty <72179940+Sw3aty-Acc@users.noreply.github.com>
2024-09-21 10:10:48 +0300

    chore: add dataclass Collection

--- src/shellui/core/typings.py
+++ src/shellui/core/typings.py
@@ -1,5 +1,6 @@
-from .overloads import dataclass, field
+from dataclasses import dataclass, field
 from typing import *
+from .interfaces import *
 
 
 @dataclass
@@ -11,3 +12,54 @@ class Buffer:
         if self.kwargs is not None:
             for name, key in self.kwargs.items():
                 setattr(self, name, key)
+
+
+@dataclass
+class Collection(list):
+    def __getitem__(self, item):
+        if isinstance(item, slice):
+            return self[slice[0]:slice[1]] if slice[1] > slice[0] else reversed(self)[slice[1]:slice[0]]
+
+    def append(self, element):
+        if not isinstance(element, BaseElementInterface):
+            raise TypeError(f"Expected type 'BaseElement', got '{type(element)}' instead")
+        super().append(element)
+
+    def extend(self, iterable):
+        for item in iterable:
+            self.append(item)
+
+    def insert(self, index, element):
+        if not isinstance(element, BaseElementInterface):
+            raise TypeError(f"Expected type 'BaseElement', got '{type(element)}' instead")
+        super().insert(index, element)
+
+    def set_attribute(self,
+                      item: str,
+                      value: Any,
+                      _lambda: Callable = lambda element: True) -> Self:
+        return_list: Collection = Collection()
+        for element in self:
+            if _lambda(element):
+                setattr(element, item, value)
+                return_list.append(element)
+        return return_list
+
+    def get_collection(self,
+                      _lambda: Callable = lambda element: True) -> Self:
+        return_list: Collection = []
+        for index, element in enumerate(self):
+            if _lambda(element):
+                return_list.append(element)
+        return return_list
+
+    def run_method(self,
+                   method: str,
+                   _lambda: Callable = lambda element: True,
+                   args: List = None,
+                   kwargs: dict = None) -> List[Any]:
+        args, kwargs = args or [], kwargs or {}
+        return_list: List[Any] = []
+        for element in self.get_collection(_lambda):
+            return_list.append(getattr(element, method)(*args, **kwargs))
+        return return_list
