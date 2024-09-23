0ba2c8a30350048cf531f76626f8da0b2a35a229
Sw3aty <72179940+Sw3aty-Acc@users.noreply.github.com>
2024-09-23 12:59:41 +0300

    chore(__version__): change script version

--- src/shellui/__init__.py
+++ src/shellui/__init__.py
@@ -2,16 +2,15 @@
 shellui - library simplifies the creation of TUI (Text User Interface)
           in the terminal. Offers a clean and intuitive architecture to
           help you build interactive applications effortlessly.
-            Credits:
+            Contacts:
         Author - highofolly
         Source - https://github.com/highofolly/shellui
          Email - sw3atyspace@gmail.com
         GitHub - https://github.com/highofolly
-Discord Server - https://discord.com/invite/jchJKYqNmK
        Youtube - https://www.youtube.com/@sw3aty702
 """
 
-__version__ = "0.1b1"
+__version__ = "0.1"
 
 import logging
 
