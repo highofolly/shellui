2b4ca70aab42ff83b342d1da42cb7b3e9c386ff0
Sw3aty <72179940+Sw3aty-Acc@users.noreply.github.com>
2024-09-29 20:17:31 +0300

    feat: add ElementState, ActiveWidget and StaticWidget classes
    change hierarchy of Label and Button classes
    change HLayout.key_pressed method
    add HLayout.update method
    delete HLayout.adjust method
    delete Button.is_active, Button.update, Button.on_click methods
    delete Label.is_active method

--- src/shellui/ui/board.py
+++ src/shellui/ui/board.py
@@ -1,31 +1,65 @@
 from . import AbstractWidget, AbstractLayout
 from .. import logging
-from ..core import curses, List, Any, Buffer
+from ..core import curses, List, Any, Union, Buffer
+from enum import Enum
 
 
-class Label(AbstractWidget):
+class ElementState(Enum):
+    INACTIVE = 0
+    ACTIVE = 1
+    SELECTED = 2
+
+
+class ActiveWidget(AbstractWidget):
+    def __init__(self, *args, **kwargs):
+        super(ActiveWidget, self).__init__(*args, **kwargs)
+        self.state: ElementState = ElementState.INACTIVE
+        self.event.create.on_click(self.on_click)
+        self.event.create.on_key_press(self.on_key_press)
+
+    def update(self) -> Any: ...
+    def on_click(self) -> Any: ...
+    def on_key_press(self, key_char) -> Any: ...
+
+    def key_pressed(self, key_char) -> Any:
+        if key_char == 10:
+            return self.event.call.on_click()
+        else:
+            return self.event.call.on_key_press(key_char)
+
+    def select(self):
+        self.state = ElementState.SELECTED
+
+    def deselect(self):
+        self.state = ElementState.INACTIVE
+
+
+class StaticWidget(AbstractWidget):
+    def __init__(self, *args, **kwargs):
+        super(AbstractWidget, self).__init__(*args, **kwargs)
+        self.state: ElementState = ElementState.INACTIVE
+
+    def update(self) -> Any: ...
+
+    def key_pressed(self, key_char) -> Any: ...
+
+
+class Label(StaticWidget):
     def __init__(self, *args, **kwargs):
         super(Label, self).__init__(*args, **kwargs)
         self.text = kwargs.pop("text")
-        self.state = False
-        self.event.create.is_active(self.is_active)
 
-    def is_active(self): return False
-    def output(self): return self.text
+    def output(self):
+        return self.text
 
 
-class Button(Label):
+class Button(ActiveWidget):
     def __init__(self, *args, **kwargs):
         super(Button, self).__init__(*args, **kwargs)
-        self.event.create.on_click(self.on_click)
-        self.state = True
-        self.event.create.key_pressed(
-            lambda key_char: self.event.call.on_click() if key_char == curses.KEY_ENTER else None)
+        self.text = kwargs.pop("text")
 
-    def is_active(self): return True
-
-    def update(self) -> Any: ...
-    def on_click(self) -> None: ...
+    def output(self):
+        return self.text if self.state == ElementState.INACTIVE else f"> {self.text}"
 
 
 class HLayout(AbstractLayout):
@@ -35,15 +69,21 @@ class HLayout(AbstractLayout):
         self.UP_KEYS = [curses.KEY_UP, curses.KEY_LEFT]
         self.DOWN_KEYS = [curses.KEY_DOWN, curses.KEY_RIGHT]
 
+    def update(self) -> Any:
+        self.elements.get_elements_collection(lambda element: isinstance(element, ActiveWidget))[self.cursor].select()
+        return self.elements.run_elements_method("update")
+
     def key_pressed(self, key_char: int) -> Any:
-        self.elements.set_attribute("state", False)
+        self.elements.set_elements_attribute("state", ElementState.INACTIVE)
         if key_char in self.UP_KEYS:
-            self.cursor -= 1
+            self.cursor = self.cursor - 1 if self.cursor > 0 else len(self.elements) - 1
         elif key_char in self.DOWN_KEYS:
-            self.cursor += 1
+            self.cursor = self.cursor + 1 if self.cursor < len(self.elements) - 1 else 0
         else:
             return self.elements[self.cursor].event.call.key_pressed(key_char)
-        self.elements.get_collection(lambda element: element.is_active())[self.cursor].state = True
 
-    def adjust(self, buffer):
-        return f"{buffer.method()}\n"
+    def output(self) -> Union[str, List[Buffer]]:
+        ret: List[Buffer] = []
+        for element in self.elements:
+            ret.append(element.event.call.build())
+        return ret
