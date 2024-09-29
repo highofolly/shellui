c02c58ba18aaa1458ab524f18075dcbc6d3ab123
Sw3aty <72179940+Sw3aty-Acc@users.noreply.github.com>
2024-09-29 20:09:57 +0300

    refactor: move methods between parent and child classes in ui.abstracts module
    AbstractWidget.output method to BaseElement class
    change BaseElement.build method
    change AbstractLayout.add_element method
    delete adjust, key_pressed, update, build methods in AbstractLayout class

--- src/shellui/ui/abstracts.py
+++ src/shellui/ui/abstracts.py
@@ -1,66 +1,39 @@
 from .. import logging
-from ..core import BaseElementInterface, EventManager, Buffer, Collection, Tuple, List, Any, abstractmethod
+from ..core import BaseElementInterface, EventManager, Buffer, Collection, Tuple, List, Any, Union, abstractmethod
 
 
 class BaseElement(BaseElementInterface):
     def __init__(self, *args, **kwargs):
         self.event = EventManager()
-        self.event.create.build(self.build)
-        self.event.create.update(self.update)
-        self.event.create.key_pressed(lambda: None)
         self.position = kwargs.pop("position", [0, 0])
+        self.event.create.update(self.update)
+        self.event.create.key_pressed(self.key_pressed)
+        self.event.create.output(self.output)
+        self.event.create.build(self.build)
         logging.debug(f"CREATE CLASS <{self.__class__.__name__}> (agrs={args}, kwargs={kwargs})")
 
     @abstractmethod
-    def build(self) -> Buffer:
-        raise NotImplementedError
-
+    def update(self) -> Any: raise NotImplementedError
     @abstractmethod
-    def update(self) -> Any:
-        raise NotImplementedError
+    def key_pressed(self, key_char) -> Any: raise NotImplementedError
+    @abstractmethod
+    def output(self) -> Union[str, List[Buffer]]: raise NotImplementedError
+
+    def build(self) -> Buffer:
+        return Buffer(self.event.call.output, self.position)
 
 
 class AbstractWidget(BaseElement):
     def __init__(self, *args, **kwargs):
         super(AbstractWidget, self).__init__(*args, **kwargs)
-        self.event.create.output(self.output)
-
-    @abstractmethod
-    def output(self) -> str:
-        raise NotImplementedError
-
-    def build(self) -> Buffer:
-        return Buffer(self.event.call.output, self.position)
 
 
 class AbstractLayout(BaseElement):
     def __init__(self, *args, **kwargs):
         super(AbstractLayout, self).__init__(*args, **kwargs)
         self.elements: Collection = Collection()
-        self.event.create.adjust(self.adjust)
-        self.event.create.key_pressed(self.key_pressed)
-
-    @abstractmethod
-    def adjust(self, buffer: Buffer) -> str:
-        raise NotImplementedError
-
-    def key_pressed(self, key_char: int) -> Any: ...
-
-    def update(self) -> Any:
-        return self.elements.run_method("update")
 
     def add_element(self, element: BaseElement, position: Tuple[int, int] = None) -> BaseElement:
-        element.position = position or [0, 0]
+        element.position = position or element.position
         self.elements.append(element)
         return element
-
-    def build(self) -> Buffer:
-        def create_buffer_list() -> List[Buffer]:
-            ret: List[Buffer] = []
-            for element in self.elements:
-                element_buffer = element.event.call.build()
-                ret.append(Buffer(method=lambda: self.event.call.adjust(element_buffer),
-                                  position=element_buffer.position))
-            return ret
-
-        return Buffer(create_buffer_list, self.position)
