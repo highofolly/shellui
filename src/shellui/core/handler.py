dc872a0f50a7437215c881f6b4ad79412e0aec6b
Sw3aty <72179940+Sw3aty-Acc@users.noreply.github.com>
2024-09-30 16:17:38 +0300

    refactor: change EventUnit class
    now __call__ method shortens passed args and kwargs for debug message
    now __init__ method saves passed parent class in EventUnit.parent attribute

--- src/shellui/core/handler.py
+++ src/shellui/core/handler.py
@@ -3,16 +3,28 @@ from .. import logging
 
 
 class EventUnit:
+    def __init__(self, parent):
+        self.parent: object = parent
+
     def __setfunc__(self, func: Callable = None):
         self.func: Callable = func
 
     def __call__(self, *args, **kwargs):
-        logging.debug(f"CALL FUNCTION <{self.func.__name__}> (agrs={args}, kwargs={kwargs})")
+        args_str = ', '.join(repr(arg) for arg in args[:2]) or None
+        if len(args) > 2:
+            args_str += ', ...'
+
+        kwargs_items = list(kwargs.items())
+        kwargs_str = ', '.join(f'{key}: {repr(value)}' for key, value in kwargs_items[:2]) or None
+        if len(kwargs_items) > 2:
+            kwargs_str += ', ...'
+
+        logging.debug(f"CLASS <{self.parent.__class__.__name__}> CALLS <{self.func.__name__}> (agrs={args_str}, kwargs={kwargs_str})")
         return self.func(*args, **kwargs)
 
 
 class EventManager:
-    def __init__(self):
+    def __init__(self, parent):
         class Create:
             def __init__(self, cls: EventManager):
                 self.cls = cls
@@ -22,7 +34,7 @@ class EventManager:
 
         class Call:
             def __newattr__(self, name):
-                setattr(self, name, EventUnit())
+                setattr(self, name, EventUnit(parent))
                 return getattr(self, name).__setfunc__
 
         self.create = Create(self)
