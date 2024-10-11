32d0d19b5417c013012c25d7df7f696d682bbf2b
Sw3aty <72179940+Sw3aty-Acc@users.noreply.github.com>
2024-10-11 18:29:14 +0300

    build: added metadata in form of headers

--- src/shellui/__init__.py
+++ src/shellui/__init__.py
@@ -1,16 +1,17 @@
-"""
-shellui - library simplifies the creation of TUI (Text User Interface)
-          in the terminal. Offers a clean and intuitive architecture to
-          help you build interactive applications effortlessly.
-            Contacts:
-        Author - highofolly
-        Source - https://github.com/highofolly/shellui
-         Email - sw3atyspace@gmail.com
-        GitHub - https://github.com/highofolly
-       Youtube - https://www.youtube.com/@sw3aty702
-"""
+# shellui - library simplifies the creation of TUI (Text User Interface)
+#           in the terminal. Offers a clean and intuitive architecture to
+#           help you build interactive applications effortlessly.
+#             Contacts:
+#         Author - highofolly
+#         Source - https://github.com/highofolly/shellui
+#          Email - sw3atyspace@gmail.com
+#         GitHub - https://github.com/highofolly
+#        Youtube - https://www.youtube.com/@sw3aty702
 
-__version__ = "0.3.0b1"
+__author__ = "highofolly"
+__license__ = "MIT"
+__version__ = "0.3.0"
+__email__ = "sw3atyspace@email.com"
 
 import logging
 
