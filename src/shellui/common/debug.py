3a802cc6ce7c918007b3cdf727b449a72bbb36b6
Sw3aty <72179940+Sw3aty-Acc@users.noreply.github.com>
2024-10-13 10:33:35 +0300

    refactor(debug): debug_start method

--- src/shellui/common/debug.py
+++ src/shellui/common/debug.py
@@ -19,20 +19,22 @@ CREATE = 7
 logging.create = create_level(CREATE, "CREATE")
 
 
-def debug_start(file_name: str = "shellui_debug.log", logging_level: int = logging.DEBUG, save_critical_error: bool = True):
-    if save_critical_error:
-        def exception_handler(exc_type, exc_value, exc_traceback):
-            logging.critical(exc_value)
-            if exc_traceback is not None:
-                import traceback
-                import subprocess
-                open("critical_error.log", "w").write(''.join(traceback.format_tb(exc_traceback)))
-                subprocess.Popen(
-                    "start cmd /c powershell.exe -Command type critical_error.log; Write-Host '\033[31mCritical error log is saved to critical_error.log file\033[0m'; pause",
-                    shell=True)
+def debug_start(logging_filename: str = "shellui_debug.log",
+                logging_level: int = CREATE,
+                logging_format: str = "%(asctime)s - %(levelname)s - %(filename)s : %(message)s\n",
+                save_traceback: bool = True) -> None:
 
-        import sys
-        sys.excepthook = exception_handler
+    def exception_handler(exc_type, exc_value, exc_traceback):
+        logging.critical(exc_value)
+        if exc_traceback is not None and save_traceback:
+            import traceback
+            import subprocess
+            open("traceback.log", "w").write(''.join(traceback.format_tb(exc_traceback)) + f"\n{exc_value}")
+            subprocess.Popen(
+                "start cmd /c powershell.exe -Command type traceback.log; Write-Host '\033[31mError traceback log is saved to traceback.log file\033[0m'; pause",
+                shell=True)
 
-    logging.basicConfig(level=logging_level, filename=file_name, filemode="w",
-                        format="%(asctime)s - %(levelname)s - %(filename)s : %(message)s\n")
+    import sys
+    sys.excepthook = exception_handler
+
+    logging.basicConfig(level=logging_level, filename=logging_filename, filemode="w", format=logging_format)
